fa732b179709144cd9aee28a9f6f9cd4807bfa1f
jdk1997 <janamejay.keskar@gmail.com>
2018-01-05 21:03:25 +0530

    first commit

---
+++ score.py
@@ -0,0 +1,31 @@
+import notify2
+import time
+from pycricbuzz import Cricbuzz
+
+ICON = '/home/dell/Downloads/beautiful-fantasy-cricket-bat-and-ball-hd-free-wallpapers-640x360.jpg'
+
+cric = Cricbuzz()
+matches = cric.matches()
+scores = []
+notify2.init('Cricket updates')
+notif = notify2.Notification(None, icon = ICON)
+notif.set_urgency(notify2.URGENCY_NORMAL)
+notif.set_timeout(10000)
+
+for match in matches:
+	if match['mchstate'] != 'preview':
+		scores.append(cric.livescore(match['id']))
+
+for i in range(len(scores)):
+	score = scores[i]
+	batting = score['batting']['score']
+	batting_dets = batting[0]
+	batting_team = score['batting']['team']
+	bowling = score['bowling']['team']
+	if score['matchinfo']['mchstate'] == 'Result':
+		notif.update(score['matchinfo']['mchdesc'], score['matchinfo']['status'])
+	else:
+		notif.update(score['matchinfo']['srs'] + '  ' + batting_team + '  ' + batting_dets['desc'] + '  ' + 'Score: ' + batting_dets['runs'] + '/' + batting_dets['wickets'] + '		' + bowling + '  ' + 'Overs: ' + batting_dets['overs'])	
+	notif.show()
+	notif.set_timeout(10000)
+	time.sleep(5)
