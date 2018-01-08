f7853cc442aef7833def1f12e9188292659e56b8
Janamejay Keskar <janamejay.keskar@gmail.com>
2018-01-08 20:02:14 +0530

    Update score.py

--- score.py
+++ score.py
@@ -7,37 +7,48 @@ ICON = '/home/dell/Downloads/beautiful-fantasy-cricket-bat-and-ball-hd-free-wall
 cric = Cricbuzz()
 matches = cric.matches()
 scores = []
+
 notify2.init('Cricket updates')
 notif = notify2.Notification(None, icon = ICON)
 notif.set_urgency(notify2.URGENCY_NORMAL)
-notif.set_timeout(10000)
+notif.set_timeout(100000)
 
 for match in matches:
-	if match['mchstate'] == 'inprogress' or match['mchstate'] == 'Result' or match['mchstate'] == 'complete':
-		scores.append(cric.livescore(match['id']))
-	elif match['mchstate'] == 'stump' or match['mchstate'] == 'stumps':
-		notif.update(match['srs'], 'At stumps: ' + match['status'])
-		notif.show()
-	elif match['mchstate'] == 'lunch':
-		notif.update(match['srs'], 'At lunch: ' + match['status'])
-		notif.show()
-	elif match['mchstate'] == 'dinner':
-		notif.update(match['srs'], 'At dinner: ' + match['status'])
+	if match['mchstate'] == 'inprogress' or match['mchstate'] == 'Result':
+		try:
+			scores.append(cric.livescore(match['id']))
+		except KeyError:
+			if match['mchstate'] == 'Result':
+				notif.update(match['srs'] + ', ' + match['mnum'], match['mchdesc'] + '\n' + match['status'])
+				notif.show()	
+	elif match['mchstate'] == 'stump':
+		notif.update(match['srs'], '\nAt stumps: ' + match['status'])
 		notif.show()
 	elif match['mchstate'] == 'tea':
-		notif.update(match['srs'], 'At tea: ' + match['status'])
+		notif.update(match['srs'], '\nAt tea: ' + match['status'])
 		notif.show()
-		
+	elif match['mchstate'] == 'lunch':
+		notif.update(match['srs'], '\nAt lunch: ' + match['status'])
+		notif.show()
+	elif match['mchstate'] == 'dinner':
+		notif.update(match_dets['srs'], '\nAt dinner: ' + match_dets['status'])
+		notif.show()
+	elif match['mchstate'] == 'rain':
+		notif.update(match['srs'], match['status'] + '\n' + 'Play stopped due to rain.')
+		notif.show()
+	elif match['mchstate'] == 'preview':
+		notif.update('Up next ' + match['srs'], match['mnum'] + ' ' + match['status'])
+		notif.show()
+	elif match['mchstate'] == 'complete':
+		notif.update(match['srs'] + ', ' + match['mnum'], match['mchdesc'] + '\n' + match['status'])
+		notif.show()
+
 for score in scores:
 	batting = score['batting']['score']
 	batting_dets = batting[0]
 	batting_team = score['batting']['team']
 	bowling = score['bowling']['team']
-	if score['matchinfo']['mchstate'] == 'Result' or mat:
-		notif.update(score['matchinfo']['mchdesc'], score['matchinfo']['status'])
-	else:
-		notif.update(score['matchinfo']['srs'] + '  ' + batting_team + '  ' + batting_dets['desc'] + '  ' + 'Score: ' + batting_dets['runs'] + '/' + batting_dets['wickets'] + '		' + bowling + '  ' + 'Overs: ' + batting_dets['overs'], score['matchinfo']['status'])	
+	notif.update(score['matchinfo']['srs'], score['matchinfo']['mnum'] + '\n' + batting_team + ' ' + batting_dets['desc'] + '  ' + 'Score: ' + batting_dets['runs'] + '/' + batting_dets['wickets'] + '	' + bowling + '  ' + 'Overs: ' + batting_dets['overs'] + '\n' + score['matchinfo']['status'])	
 	notif.show()
-	notif.set_timeout(10000)
 	time.sleep(5)
 notif.close()
