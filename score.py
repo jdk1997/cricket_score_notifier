2f6b7ee605581c1a8c46cb7385e4bfa38812acdf
Janamejay Keskar <janamejay.keskar@gmail.com>
2018-01-08 12:47:34 +0530

    Update score.py

--- score.py
+++ score.py
@@ -13,7 +13,7 @@ notif.set_urgency(notify2.URGENCY_NORMAL)
 notif.set_timeout(10000)
 
 for match in matches:
-	if match['mchstate'] == 'inprogress' or match['mchstate'] == 'Result':
+	if match['mchstate'] == 'inprogress' or match['mchstate'] == 'Result' or match['mchstate'] == 'complete':
 		scores.append(cric.livescore(match['id']))
 	elif match['mchstate'] == 'stump' or match['mchstate'] == 'stumps':
 		notif.update(match['srs'], 'At stumps: ' + match['status'])
@@ -34,7 +34,7 @@ for i in range(len(scores)):
 	batting_dets = batting[0]
 	batting_team = score['batting']['team']
 	bowling = score['bowling']['team']
-	if score['matchinfo']['mchstate'] == 'Result':
+	if score['matchinfo']['mchstate'] == 'Result' or mat:
 		notif.update(score['matchinfo']['mchdesc'], score['matchinfo']['status'])
 	else:
 		notif.update(score['matchinfo']['srs'] + '  ' + batting_team + '  ' + batting_dets['desc'] + '  ' + 'Score: ' + batting_dets['runs'] + '/' + batting_dets['wickets'] + '		' + bowling + '  ' + 'Overs: ' + batting_dets['overs'], score['matchinfo']['status'])	
